2f10da09e5ff266efca11f531265fd25b23ae0cc
candidato987 <candidato987@mail.com>
2022-11-20 20:03:05 -0500

    console editar paciente #2

--- controllers/especie.controller.js
+++ controllers/especie.controller.js
@@ -42,7 +42,7 @@ const sqlSetEspecie = (conexion, especie, callback) => {
             + `nombre = "${especie['nombre']}" , `
             + `caracteristicas = "${especie['caracteristicas']}" , `
             + `imagen = "${especie['imagen']}"  WHERE id = ${especie['id']} `;
-            log( '**editado mascota');
+            console.log( '**editado mascota');
     } else {
         strSql =
             " INSERT INTO especie (nombre, caracteristicas, imagen) " +
